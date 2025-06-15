1d0f7ee0dde3032e1d4b2e1562d44e299ca597a0
jaiminsavaliya316 <jaiminsavaliya317@gmail.com>
2025-06-15 19:19:03 -0400

    changed home page from default one, created routes and some styles, created responsive nav bar which with colapsing functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './Home'
+import Profile from './Profile';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route /* Home Page */ path="/" element={<Navigate to="home"/>} />
+        <Route /* Home Page */ path="/home" element={<Home/>} />
+        <Route /* Profile Page */ path="/profile" element={<Profile/>} />
+      </Routes>
+    </HashRouter>
   )
 }
 
